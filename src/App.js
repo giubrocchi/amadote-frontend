db7c4b69bad1acfc0ab948810c4de371229b1950
Giulia Brocchi <jujubrocchi@gmail.com>
2023-04-07 21:01:09 -0300

    image upload test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const handleImageUpload = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:8080/api/tutorials', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ file: image }),
+    });
+
+    const { imageUrl } = await response.json();
+    setImageUrl(imageUrl);
+  };
+
+  const handleImageChange = (e) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Image Uploader</h1>
+      <form onSubmit={handleImageUpload}>
+        <input type="file" onChange={handleImageChange} />
+        <button type="submit">Upload</button>
+      </form>
+      {imageUrl && (
+        <div>
+          <img src={imageUrl} alt="Uploaded image" />
+          <p>Image URL: {imageUrl}</p>
+        </div>
+      )}
     </div>
   );
 }
