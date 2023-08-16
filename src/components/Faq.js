6fefb4823477c03a7c64822a6510738d61582296
Matheus <gtacampinas@hotmail.com>
2023-08-15 23:43:55 -0300

    Adjustment Faq and Post + CSS

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,28 +1,30 @@
 import React from 'react';
 
-export default function Faq(){
-    const faqData = [
-        {
-          question: 'Qual é o processo de adoção?',
-          answer: 'O processo de adoção é.....'
-        },
-        {
-          question: 'Como faço para adotar um animal?',
-          answer: 'Para adotar um animal é necessário... ...'
-        },
-      ];
-    
-      return (
-        <div className='faqBody'>
-          <h1 id="PergTitle">Perguntas Frequentes</h1>
-          <div className='faqList'>
-            {faqData.map((item, index) => (
-              <div className='faqItem' key={index}>
-                <h2 className='faqQuestion'>{item.question}</h2>
-                <p className='faqAnswer'>{item.answer}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      );
-    }
+export default function Faq() {
+  const faqData = [
+    {
+      question: 'Qual é o processo de adoção?',
+      answer: 'O processo de adoção é.....'
+    },
+    {
+      question: 'Como faço para adotar um animal?',
+      answer: 'Para adotar um animal é necessário... ...'
+    },
+  ];
+
+  const faqItems = faqData.map((item, index) => (
+    <div className='faqItem' key={index}>
+      <h2 className='faqQuestion'>{item.question}</h2>
+      <p className='faqAnswer'>{item.answer}</p>
+    </div>
+  ));
+
+  return (
+    <div className='faqBody'>
+      <h1 id="PergTitle">Perguntas Frequentes</h1>
+      <div className='faqList'>
+        {faqItems}
+      </div>
+    </div>
+  );
+}
