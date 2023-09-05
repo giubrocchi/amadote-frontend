56b3d1cbe8d28713be41523c6e6e5135f52ff82c
Giulia Brocchi <jujubrocchi@gmail.com>
2023-09-05 17:05:21 -0300

    [UC-22] Listar pedidos de adoção - Front-end [UC-13] Listar adoções realizadas - Front-end

--- src/components/adoption/AdoptionList.js
+++ src/components/adoption/AdoptionList.js
@@ -125,6 +125,7 @@ export default function AdoptionList() {
                     </div>
                   </div>
                 );
+              return;
             })}
           </>
         )}
@@ -158,6 +159,7 @@ export default function AdoptionList() {
                     </div>
                   </div>
                 );
+              return;
             })}
           </>
         )}
